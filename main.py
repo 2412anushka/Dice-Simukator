7eb6ddfd1b1e0ef214aa6bbfa7f40d4e7879d138
Anushka <anushkasy2003@gmail.com>
2023-12-31 14:17:10 +0530

    main and img files uploaded

---
+++ main.py
@@ -0,0 +1,33 @@
+
+import tkinter as tk
+from PIL import Image,ImageTk
+
+import random
+
+root=tk.Tk()
+root.geometry("2000x2000")
+root.title("Dice Roller Simulator")
+
+dice=["DiceImg\d1.png","DiceImg\d2.png","DiceImg\d3.png","DiceImg\d4.png","DiceImg\d5.png","DiceImg\d6.png"]
+image1=ImageTk.PhotoImage(Image.open(random.choice(dice)))
+image2=ImageTk.PhotoImage(Image.open(random.choice(dice)))
+
+
+label1=tk.Label(root,image=image1)
+label2=tk.Label(root,image=image2)
+
+label1.place(x=40,y=100)
+label2.place(x=700,y=100)
+
+def dice_roll():
+    image1=ImageTk.PhotoImage(Image.open(random.choice(dice)))
+    label1.configure(image=image1)
+    label1.image=image1
+
+    image2=ImageTk.PhotoImage(Image.open(random.choice(dice)))
+    label2.configure(image=image2)
+    label2.image=image2
+
+button=tk.Button(root,text="Roll Dice",bg="black",fg="white",font="Verdana 20 bold",highlightcolor="black",highlightbackground="white",relief="raised",command=dice_roll)
+button.place(x=545,y=30)
+root.mainloop()
